5d9561eb40d5975e86f0b8b9bfa74c7d10cd2c37
b12k <bogdan.kolesnyk@gmail.com>
2024-02-20 01:39:11 +0100

    loopback

--- src/client/pages/page-home.vue
+++ src/client/pages/page-home.vue
@@ -1,20 +1,22 @@
 <script lang="ts" setup>
   import { onMounted, ref,watch } from 'vue';
   import { useContextStore } from '../store';
-  import { injectTrackingIframe } from '../services';
+  import { useRouter } from 'vue-router';
 
   const context = useContextStore();
   const uuid = ref('');
-  const checked = ref('');
+
+  const trackMe = () => {
+    window.location.href = `${context.xSiteIframe}/loopback?to=${window.location.href}`
+  }
+
+  const router = useRouter();
 
   onMounted(async () => {
-    const { uuid: storedUuid, checked: storedChecked, iframe } = await injectTrackingIframe(context.xSiteIframe);
-    uuid.value = storedUuid;
-    checked.value = storedChecked || '';
-
-    watch(checked, (value: string) => {
-      iframe.contentWindow?.postMessage({ checked: value }, '*');
-    });
+    if (typeof context.query.uuid === 'string') {
+      uuid.value = context.query.uuid;
+      router.replace({ name: 'home' });
+    }
   })
 
 </script>
@@ -22,15 +24,6 @@
   <div class="d-flex flex-column h-100 align-items-center justify-content-center">
     <h1>{{ context.icon }}</h1>
     <p>Your ID: {{ uuid }}</p>
-    <div class="d-grid gap-2">
-      <input type="radio" class="btn-check" value="green" name="btnradio" id="btnradio1" autocomplete="off" v-model="checked">
-      <label class="btn btn-outline-success" for="btnradio1">Green</label>
-
-      <input type="radio" class="btn-check" value="yellow" name="btnradio" id="btnradio2" autocomplete="off" v-model="checked">
-      <label class="btn btn-outline-warning" for="btnradio2">Yellow</label>
-
-      <input type="radio" class="btn-check" value="red" name="btnradio" id="btnradio3" autocomplete="off" v-model="checked">
-      <label class="btn btn-outline-danger" for="btnradio3">Red</label>
-    </div>
+    <button type="button" class="btn btn-primary" @click="trackMe">Track me</button>
   </div>
 </template>
